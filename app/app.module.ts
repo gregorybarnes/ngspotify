226f8afb2079c02de307fcfb60aa1fc3f4e08a45
Michael Barnes <michael.barnes@twc-contractor.com>
2016-09-23 09:32:12 -0600

    Implemented routing for about and search components

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,26 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
+import { routing } from './app.routing';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from  './components/about/about.component';
+import { SearchComponent } from  './components/search/search.component';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    SearchComponent
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- app/components/navbar/navbar.component.ts
+++ app/components/navbar/navbar.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'navbar',
-  template: 'navbar.component.html'
+  templateUrl: 'navbar.component.html'
 })
 export class NavbarComponent { }
