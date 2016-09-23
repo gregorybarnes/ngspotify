9e29f9b62e1e4d41881aa488c064b8603a1fc363
Michael Barnes <michael.barnes@twc-contractor.com>
2016-09-23 14:00:42 -0600

    Added text to about page and added preview song html and injection

--- app/components/album/album.component.ts
+++ app/components/album/album.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {SpotifyService} from '../../services/spotify.service';
-import {Artist} from '../../../Artist';
 import {Album} from '../../../Album';
 import { Router, ActivatedRoute } from '@angular/router';
 
